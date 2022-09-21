eadbfe628783fc124cfa6c93f71c8f89c6d4c2cc
ise999joy <113753668+ise999joy@users.noreply.github.com>
2022-09-21 13:33:55 +0300

    Add files via upload

---
+++ практическа 3(точно).py
@@ -0,0 +1,19 @@
+
+
+
+number_1 = input('Enter your first number: ')
+number_2 = input('Enter your second number: ')
+
+
+number_1 = number_1[0]
+
+number_1 = int(number_1)
+
+
+number_2 = int(number_2[0])
+
+
+result = number_1 * number_2
+
+
+print(result)
